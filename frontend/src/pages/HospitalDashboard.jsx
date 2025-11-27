aa760b36974a4a113afd5bc235e82945e8bab8e3
harsh shinde <shindeharsh.dev@gmail.com>
2025-11-27 23:04:05 +0530

    Added file rename and 30s countdown done updatesa

--- frontend/src/pages/HospitalDashboard.jsx
+++ frontend/src/pages/HospitalDashboard.jsx
@@ -16,7 +16,7 @@ import logoMain from '../assets/logo_final.png';
 import imgAtropine from '../assets/medicines/Atropine.jpg';
 import imgActrapid from '../assets/medicines/Actrapid_Plain.webp';
 // ⚠️ CHANGED TO .png (Make sure the file in your folder is Dopamine.png)
-import imgDopamine from '../assets/medicines/Dopamine.jpg'; 
+import imgDopamine from '../assets/medicines/Dopamine_med.jpg'; 
 import imgAvil from '../assets/medicines/Avil.webp';
 import imgAdrenaline from '../assets/medicines/Adranaline.webp';
 import imgDexa from '../assets/medicines/Dexa.jpg';
