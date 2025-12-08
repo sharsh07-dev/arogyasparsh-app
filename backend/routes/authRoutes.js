248f24c5671df312fce75a1ce89309065d380d7a
harsh shinde <shindeharsh.dev@gmail.com>
2025-12-08 16:37:58 +0530

    Fixed inventory logic and dashboard features location

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 // Import the new functions
 const { registerUser, loginUser, forgotPassword, resetPassword, setLandingZone,seedUsers} = require("../controllers/authController");
-
+const User = require("../models/User");
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
@@ -11,4 +11,18 @@ router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.put("/set-landing-zone", setLandingZone);
 router.get("/seed", seedUsers); // New Route
+
+router.put("/update-location", async (req, res) => {
+    try {
+        const { email, coordinates } = req.body;
+        const user = await User.findOneAndUpdate(
+            { email: email },
+            { $set: { landingCoordinates: coordinates } },
+            { new: true }
+        );
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
